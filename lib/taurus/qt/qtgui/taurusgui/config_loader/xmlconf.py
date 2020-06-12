00a0c43d5fd68d6ec35eb9c91d06a7ebee3aa5d4
Stanislaw Cabala <stanislaw.cabala@gmail.com>
2020-06-12 22:36:27 +0200

    Add implementation of config loader from XML file

--- lib/taurus/qt/qtgui/taurusgui/config_loader/xmlconf.py
+++ lib/taurus/qt/qtgui/taurusgui/config_loader/xmlconf.py
@@ -23,11 +23,121 @@
 
 """"""
 
-from .abstract import AbstractConfigLoader
+from lxml import etree
+
+from ..utils import PanelDescription, ToolBarDescription, AppletDescription, ExternalApp
+from .abstract import AbstractConfigLoader, ConfigLoaderError
 
 
 __all__ = ["XmlConfigLoader"]
 
 
 class XmlConfigLoader(AbstractConfigLoader):
-    pass
+    def __init__(self, confname):
+        super(XmlConfigLoader, self).__init__(confname)
+        self._root = etree.fromstring('<root></root>')
+
+    def _get(self, nodename, default=None):
+        """
+        Helper method for getting data from XML
+        """
+        name = self._root.find(nodename)
+        if name is None or name.text is None:
+            return default
+        else:
+            return name.text
+
+    def _get_objects(self, klass):
+        """
+        Helper method to get list of objects of given Python class
+        """
+        objs = []
+        obj = self._root.find(klass.__name__)
+        if obj is not None:
+            for child in obj:
+                if child.tag == klass.__name__:
+                    child_str = etree.tostring(child, encoding='unicode')
+                    o = klass.fromXml(child_str)
+                    if o is not None:
+                        objs.append(o)
+        return objs
+
+    def load(self):
+        """
+        Get the xml root node from the xml configuration file
+        """       
+        try:
+            with open(self._confname, 'r') as xmlFile:
+                self._root = etree.fromstring(xmlFile.read())
+        except IOError as e:
+            raise ConfigLoaderError("Problem with accessing config file: " + str(e))
+        except Exception as e:
+            msg = 'Error reading the XML file: "%s"' % self._confname
+            raise ConfigLoaderError(msg)
+
+    @property
+    def app_name(self):
+        return self._get("GUI_NAME")
+
+    @property
+    def org_name(self):
+        return self._get("ORGANIZATION")
+
+    @property
+    def custom_logo(self):
+        return self._get("CUSTOM_LOGO")
+
+    @property
+    def org_logo(self):
+        return self._get("ORGANIZATION_LOGO")
+
+    @property
+    def single_instance(self):
+        return self._get("SINGLE_INSTANCE")
+
+    @property
+    def manual_uri(self):
+        return self._get("MANUAL_URI")
+
+    @property
+    def ini_file(self):
+        return self._get("INIFILE")
+
+    @property
+    def extra_catalog_widgets(self):
+        """
+        Not implemented for now
+        """
+        return []
+
+    @property
+    def synoptics(self):
+        synoptic = []
+        node = self._root.find("SYNOPTIC")
+        if (node is not None) and (node.text is not None):
+            for child in node:
+                s = child.get("str")
+                # we do not append empty strings
+                if s:
+                    synoptic.append(s)
+        return synoptic
+
+    @property
+    def console(self):
+        return self._get("CONSOLE")
+
+    @property
+    def panels(self):
+        return self._get_objects(PanelDescription)
+
+    @property
+    def toolbars(self):
+        return self._get_objects(ToolBarDescription)
+
+    @property
+    def applets(self):
+        return self._get_objects(AppletDescription)
+
+    @property
+    def external_apps(self):
+        return self._get_objects(ExternalApp)
