684ba73737b63f19c9c334d09f488bcbf8d3908d
Stanislaw Cabala <stanislaw.cabala@gmail.com>
2020-06-12 22:59:36 +0200

    Get in line with what XML file loads

--- lib/taurus/qt/qtgui/taurusgui/config_loader/jsonconf.py
+++ lib/taurus/qt/qtgui/taurusgui/config_loader/jsonconf.py
@@ -49,48 +49,48 @@ class JsonConfigLoader(AbstractConfigLoader):
 
     @property
     def app_name(self):
-        return self._data.get("guiName")
+        return self._data.get("GUI_NAME")
 
     @property
     def org_name(self):
-        return self._data.get("organization")
+        return self._data.get("ORGANIZATION")
 
     @property
     def custom_logo(self):
-        return self._data.get("customLogo")
+        return self._data.get("CUSTOM_LOGO")
 
     @property
     def org_logo(self):
-        return self._data.get("organizationLogo")
+        return self._data.get("ORGANIZATION_LOGO")
 
     @property
     def single_instance(self):
-        return self._data.get("singleInstance")
+        return self._data.get("SINGE_INSTANCE")
 
     @property
     def manual_uri(self):
-        return self._data.get("manual")
+        return self._data.get("MANUAL_URI")
 
     @property
     def ini_file(self):
-        return self._data.get("iniFile")
+        return self._data.get("INIFILE")
 
     @property
     def extra_catalog_widgets(self):
-        return self._data.get("extraCatalogWidgets", [])
+        return self._data.get("EXTRA_CATALOG_WIDGETS", [])
 
     @property
     def synoptics(self):
-        return self._data.get("synoptic", [])
+        return self._data.get("SYNOPTIC", [])
 
     @property
     def console(self):
-        return self._data.get("console")
+        return self._data.get("CONSOLE")
 
     @property
     def panels(self):
         panels = []
-        for p in self._data.get("panels", []):
+        for p in self._data.get("PanelDescriptions", []):
             if isinstance(p, dict):
                 panels.append(PanelDescription(**p))
         return panels
@@ -98,7 +98,7 @@ class JsonConfigLoader(AbstractConfigLoader):
     @property
     def toolbars(self):
         toolbars = []
-        for t in self._data.get("toolbars", []):
+        for t in self._data.get("ToolBarDescriptions", []):
             if isinstance(t, dict):
                 toolbars.append(ToolBarDescription(**t))
         return toolbars
@@ -106,7 +106,7 @@ class JsonConfigLoader(AbstractConfigLoader):
     @property
     def applets(self):
         applets = []
-        for a in self._data.get("applets", []):
+        for a in self._data.get("AppletDescriptions", []):
             if isinstance(a, dict):
                 applets.append(AppletDescription(**a))
         return applets
@@ -114,7 +114,7 @@ class JsonConfigLoader(AbstractConfigLoader):
     @property
     def external_apps(self):
         external_apps = []
-        for e in self._data.get("externalApps", []):
+        for e in self._data.get("ExternalApps", []):
             if isinstance(e, dict):
                 external_apps.append(PanelDescription(**e))
         return external_apps
